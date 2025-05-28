c0a27e4a75594846cc1de4d09b353791c45eb9fe
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-28 23:17:11 +0900

    feat: Add Nav items

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,14 +1,148 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/common/Navbar";
-import Header from "../components/common/Header";
+import { Outlet, Link as RouterLink, useLocation } from "react-router-dom";
+import { useState } from "react";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu as MenuIcon,
+  Dashboard as DashboardIcon,
+  People as PeopleIcon,
+  Business as BusinessIcon,
+  EventAvailable as EventAvailableIcon,
+  EventBusy as EventBusyIcon,
+} from "@mui/icons-material";
 
-function Layout() {
-  return (
+const drawerWidth = 240;
+
+const menuItems = [
+  { text: "Dashboard", icon: <DashboardIcon />, path: "/" },
+  { text: "Members", icon: <PeopleIcon />, path: "/members" },
+  { text: "Departments", icon: <BusinessIcon />, path: "/departments" },
+  { text: "Attendance", icon: <EventAvailableIcon />, path: "/attendance" },
+  { text: "Leave", icon: <EventBusyIcon />, path: "/leave" },
+];
+
+function Layout({ children, mobileOpen, handleDrawerToggle }) {
+  const theme = useTheme();
+  const location = useLocation();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const drawer = (
     <div>
-      <Header />
-      <Navbar />
-      <Outlet />
+      <Toolbar
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Typography variant="h6" noWrap component="div">
+          HR Management
+        </Typography>
+      </Toolbar>
+      <Divider />
+      <List>
+        {menuItems.map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton
+              component={RouterLink}
+              to={item.path}
+              selected={location.pathname === item.path}
+              onClick={isMobile ? handleDrawerToggle : undefined}
+            >
+              <ListItemIcon sx={{ minWidth: 40 }}>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </div>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {menuItems.find((item) => item.path === location.pathname)?.text ||
+              "HR Management"}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar /> {/* This Toolbar is for spacing below the AppBar */}
+        {children || <Outlet />}
+      </Box>
+    </Box>
   );
 }
 
